c59635714a809dbfb60f4e683ab2a6a1a13029cd
unknown <s27746@pjwstk.edu.pl>
2026-06-11 01:16:20 +0200

    HospitalContext corrected and 'OnConfiguring' option deleted

--- WebApplicationDBFirst/Program.cs
+++ WebApplicationDBFirst/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplicationDBFirst.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<HospitalContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
